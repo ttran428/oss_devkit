1ba089f89e8d0c57602ffa205738c01dc5e144ff
Teddy Tran <teddytheodoretran248@berkeley.edu>
2017-06-29 13:40:02 -0700

    added example of music features in machine-shop

--- testgit.py
+++ testgit.py
@@ -11,17 +11,15 @@ def cli():
 @cli.command()
 @click.argument("command", default = "")
 @click.argument("num", default = 0)
-#activates when user types in cmd "test hub pr"
+#activates when user types in cmd "testgit hub pr"
 def hub(command,num):
 	if command == "pr":
-		print("pulling down pr " + str(num))
+		print("pulling down pr " + str(num) + ":")
 		g = Github('')#authentification token goes here
-		g.get_user().get_repo("oss_devkit").get_pull(num)
+		pr = g.get_user("machine-shop").get_repo("music-features").get_pull(num)
+		print(pr.body)
 	else:
 		print("invalid command")
 
-#testing to get all pull requests
-# g = Github('')
-# for pulls in g.get_user().get_repo("oss_devkit").get_pulls():
 
 
