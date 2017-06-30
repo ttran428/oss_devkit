e66eed34f734b0c33f685fb535383f3296231da2
Teddy Tran <teddytheodoretran248@berkeley.edu>
2017-06-29 23:42:14 -0700

    got repo and username

--- testgit.py
+++ testgit.py
@@ -1,5 +1,4 @@
-
-import subprocess
+import subprocess 
 from github import Github 
 
 import click
@@ -15,16 +14,39 @@ def cli():
 #activates when user types in cmd "testgit hub pr"
 def hub(command,num):
 	if command == "pr":
-		print("pulling down pr " + str(num) + ":")
-		g = Github('')#authentification token goes here
-		pr = g.get_user("machine-shop").get_repo("music-features").get_pull(num)
-		print(pr.body)
+		print("pulling down pr " + str(num) + ":" )
+
+		#runs git and gets user and repo.
+		process = subprocess.Popen("git remote -v", stdout=subprocess.PIPE)
+		name = str(process.stdout.read())
+		almost = name.split(" ", 1)[0] #splits by first space to get the fetch url
+		url = almost.split("\\t", 1)[1]#takes out information("origin") before url
+		arguments = url.split(".com/")[1]
+		arguments = arguments[:len(arguments) - 4] #takes out ".git"
+		username, repo = arguments.split("/")
+		
+		try:
+			g = Github('')#authentification token goes here
+		except:
+			need 
+
+		try:
+			pr = g.get_user(username).get_repo(repo).get_pull(num)
+			print(pr.body)
+		except:
+			print("no pr found")
+		
 
 	else:
 		print("invalid command")
 
 #trying to use subprocess
-hello = subprocess.run("git remote -v")
-print(hello)
+process = subprocess.Popen("git remote -v", stdout=subprocess.PIPE)
+name = str(process.stdout.read())
+almost = name.split(" ", 1)[0] #splits by first space to get the fetch url
+url = almost.split("\\t", 1)[1]#takes out information("origin") before url
+arguments = url.split(".com/")[1]
+arguments = arguments[:len(arguments) - 4] #takes out ".git"
+username, repo = arguments.split("/")
 
 
