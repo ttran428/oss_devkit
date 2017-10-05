d3fe459a6e5b8b8f64f5d2948b4195eebe7aade2
Teddy Tran <teddytheodoretran248@berkeley.edu>
2017-10-04 22:30:14 -0700

    trying clone 3

--- test_push_pull.py
+++ test_push_pull.py
@@ -1,8 +1,9 @@
 
-
+import subprocess
 def test_git_repo():
 	subprocess.Popen(["git", "clone", "git@github.com:machine-shop/test_repo.git"])
 	subprocess.Popen(["ls"])
+	subprocess.Popen(["cd", "test_repo"])
 def test_push():
 	assert 5 == 5
 
