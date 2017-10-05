812d5223f89ef8fdba27d2312f7922a5f4584f70
Teddy Tran <teddytheodoretran248@berkeley.edu>
2017-10-05 00:03:37 -0700

    trying again

--- test_push_pull.py
+++ test_push_pull.py
@@ -1,12 +1,18 @@
 
 import subprocess
+import os
 def test_git_repo():
-	subprocess.Popen(["mkdir", "test3"])
-	subprocess.Popen(["ls"])
-	subprocess.Popen(["cd", "test3"])
-	subprocess.Popen(["git", "clone", "https://github.com/ttran428/test_repo.git"])
-	subprocess.Popen(["ls"])
-	subprocess.Popen(["cd", "test_repo"])
+	os.makedirs("test3")
+	os.chdir(os.getcwd() + "/test3")
+	subprocess.Popen(["git", "clone", "https://github.com/machine-shop/test_repo.git"])
+	print("finished cloning")
+	os.chdir(os.getcwd() + "/test_repo")
+
+	print("running new command")
+	command = subprocess.Popen(["git", "hub", "pr", "1"], stdout=subprocess.PIPE)
+	print(str(command.stdout.read()))
+
+
 def test_push():
 	assert 5 == 5
 
