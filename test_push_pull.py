354c60f1ffa0be21182a39d67caea97bc7597529
Teddy Tran <teddytheodoretran248@berkeley.edu>
2017-10-18 22:59:32 -0700

    fixed tests since added to wrong branch

--- test_push_pull.py
+++ test_push_pull.py
@@ -1,27 +1,57 @@
-
 import subprocess
 import os
-def test_git_repo():
-    os.makedirs("test3")
-    os.chdir(os.getcwd() + "/test3")
-    p = subprocess.Popen(["git", "clone", "https://github.com/machine-shop/test_repo.git"])
-    p.communicate()
-    os.chdir(os.getcwd() + "/test_repo")
+import shutil 
+def test_pr():
+	start = str(os.getcwd())
+	os.makedirs("test3")
+	os.chdir(os.getcwd() + "/test3")
+	p = subprocess.Popen(["git", "clone", "https://github.com/machine-shop/test_repo.git"])
+	p.communicate()
+	os.chdir(os.getcwd() + "/test_repo")
 
-    print("running new command")
-    #command = subprocess.Popen(["git", "hub", "pr", "1"], stdout=subprocess.PIPE, stdin = subprocess.PIPE)
-    p = subprocess.Popen(["git", "remote", "add", "ttran428", 'https://github.com/ttran428/test_repo.git'])
-    p.communicate()
-    p = subprocess.Popen(["git", "fetch", "ttran428"], stdout=subprocess.PIPE)
-    p.stdout.read()
-    p = subprocess.Popen(["git", "checkout", "-b",'pr/1', 'ttran428/test'], stdout=subprocess.PIPE)
-    assert str(p.stdout.read()) == 'Branch pr/1 set up to track remote branch test from ttran428.\n'
-    os.chdir(os.getcwd() + "/../..")
-    subprocess.Popen(["rm", "-rf", "test3"])
+	command = subprocess.Popen(["git", "hub", "pr", "1"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	assert str(command.stdout.read()) == 'Branch pr/1 set up to track remote branch test from ttran428.\n'
+	
+	# os.chdir("../..")
+	# shutil.rmtree("test3")
 
-
+def test_pr_out_of_bounds():
+	command = subprocess.Popen(["git", "hub", "pr", "3"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	assert "There is no pr with number 3." in str(command.stdout.read())
+	
 def test_push():
-    assert 5 == 5
+	# start = str(os.getcwd())
+	# os.makedirs("test3")
+	# os.chdir(os.getcwd() + "/test3")
+	# p = subprocess.Popen(["git", "clone", "https://github.com/machine-shop/test_repo.git"])
+	# p.communicate()
+	# os.chdir(os.getcwd() + "/test_repo")
 
-def test_pull():
-    assert 6 == 6
+	with open("teddy.txt", "w") as f:
+		f.write("testing testing...")
+	subprocess.Popen(["git", "add", "teddy.txt"])
+	subprocess.Popen(["git", "commit", "-m", "local tests"])
+	p = subprocess.Popen(["git", "hub", "push"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	assert str(p.stdout.read()) == ""
+
+	os.chdir("../..") #deletes all of the files 
+	shutil.rmtree("test3")
+
+def test_push_before_pull():
+	start = str(os.getcwd())
+	os.makedirs("test3")
+	os.chdir(os.getcwd() + "/test3")
+	p = subprocess.Popen(["git", "clone", "https://github.com/machine-shop/test_repo.git"])
+	p.communicate()
+	os.chdir(os.getcwd() + "/test_repo")
+
+	with open("teddy.txt", "w") as f:
+		f.write("testing testing...")
+	subprocess.Popen(["git", "add", "teddy.txt"])
+	subprocess.Popen(["git", "commit", "-m", "local tests"])
+	p = subprocess.Popen(["git", "hub", "push"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	assert "This command must be used after git hub pr" in str(p.stdout.read()) 
+
+	os.chdir("../..") #deletes all of the files 
+	shutil.rmtree("test3")
+
